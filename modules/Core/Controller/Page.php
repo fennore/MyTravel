b3f2f93d1cc473bb1add96eaa3bc8bbf0b82a22b
fennore <29664945+fennore@users.noreply.github.com>
2017-08-15 12:53:01 +0200

    Added Core page controller file

---
+++ modules/Core/Controller/Page.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace MyTravel\Core\Controller;
+
+class Page {
+  public function viewHomePage() {
+    
+  }
+  public function viewAboutPage() {
+
+  }
+  public function viewEditImagePage() {
+
+  }
+
+}
