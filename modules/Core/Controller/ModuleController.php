d12d8c519daaa5fb6a1b0e82dcd7f3e15fb1ee13
fennore <29664945+fennore@users.noreply.github.com>
2017-08-17 17:21:39 +0200

    Small Module wrapper update

--- modules/Core/Controller/ModuleController.php
+++ modules/Core/Controller/ModuleController.php
@@ -2,6 +2,8 @@
 
 namespace MyTravel\Core\Controller;
 
+use ErrorException;
+use ReflectionClass;
 use Symfony\Component\Finder\Finder;
 use MyTravel\Core\Model\Module;
 
@@ -13,6 +15,10 @@ class ModuleController {
 
   }
 
+  /**
+   * Return an array of Module instances
+   * @return array
+   */
   public static function getModules() {
     if (!(self::$self instanceof ModuleController)) {
       self::$self = new ModuleController();
@@ -24,6 +30,14 @@ class ModuleController {
     return self::$self->modules;
   }
 
+  /**
+   * Crawls the modules directory looking for valid modules.
+   * A valid module has a map with the module name under the modules directory.
+   * And a module controller class ModulenameController which
+   * implements the Mytravel\Core\ModuleControllerInterface.
+   *
+   * @return array
+   */
   private function findModules() {
     $moduleList = array();
     $finder = new Finder();
@@ -32,13 +46,41 @@ class ModuleController {
       $pathSections = preg_split('/\/|\\\\/', $item->getRelativePathname());
       $moduleName = array_shift($pathSections);
       $fileName = array_pop($pathSections);
+      $moduleControllerClass = 'MyTravel\\' . $moduleName .
+        '\Controller\\' . $moduleName . 'Controller';
       $validFile = $fileName === $moduleName . 'Controller.php';
       $validDirectory = isset($pathSections[0]) ? $pathSections[0] === 'Controller' : false;
-      if ($validFile && $validDirectory) {
+      // Add the module prefix to the autoloader
+      App::get()
+        ->addAutoloadPrefix('MyTravel\\' . $moduleName, 'modules\\' . $moduleName);
+      if ($validFile && $validDirectory && $this->validateControllerClass($moduleControllerClass)) {
         array_push($moduleList, new Module($moduleName));
       }
     }
     return $moduleList;
+  }
+
+  /**
+   * Validates the controller class when the file has been found.
+   * The controller must inherit from ModuleControllerInterface.
+   * The classname and namespace must be set according to PSR-4.
+   * @param string $moduleControllerClass
+   * @throws ErrorException
+   */
+  private function validateControllerClass($moduleControllerClass) {
+    // Check if Class actually exists and not just the file
+    if (!class_exists($moduleControllerClass)) {
+      $msg = 'Found ' . $moduleControllerClass . ' controller file but class / namespace is missing.';
+      throw new ErrorException($msg);
+    }
+    // Check if the Module Controller has proper inheritance
+    $checkClass = new ReflectionClass($moduleControllerClass);
+    $interfaceNames = $checkClass->getInterfaceNames();
+
+    if (!in_array('MyTravel\Core\ModuleControllerInterface', $interfaceNames)) {
+      $msg = $moduleControllerClass . ' should inherit from ModuleControllerInterface.';
+      throw new ErrorException($msg);
+    }
   }
 
 }
--- modules/Core/Model/Module.php
+++ modules/Core/Model/Module.php
@@ -2,9 +2,7 @@
 
 namespace MyTravel\Core\Model;
 
-use ErrorException;
 use OverflowException;
-use ReflectionClass;
 use MyTravel\Core\Controller\App;
 use MyTravel\Core\Controller\Config;
 
@@ -33,28 +31,6 @@ class Module {
       throw new OverflowException($msg);
     }
   }
-  /**
-   * Validates the controller class when the file has been found.
-   * The controller must inherit from ModuleControllerInterface.
-   * The classname and namespace must be set according to PSR-4.
-   * @param string $moduleControllerClass
-   * @throws ErrorException
-   */
-  private function validateControllerClass($moduleControllerClass) {
-    // Check if Class actually exists and not just the file
-    if (!class_exists($moduleControllerClass)) {
-      $msg = 'Found controller file for module ' . $this->name . ' but class / namespace is missing.';
-      throw new ErrorException($msg);
-    }
-    // Check if the Module Controller has proper inheritance
-    $checkClass = new ReflectionClass($moduleControllerClass);
-    $interfaceNames = $checkClass->getInterfaceNames();
-
-    if (!in_array('MyTravel\Core\ModuleControllerInterface', $interfaceNames)) {
-      $msg = 'Module ' . $this->name . ' should inherit from ModuleControllerInterface.';
-      throw new ErrorException($msg);
-    }
-  }
 
   /**
    * Load the module.
@@ -67,10 +43,6 @@ class Module {
     $moduleControllerClass = 'MyTravel\\' . $this->name .
       '\Controller\\' . $this->name . 'Controller';
     $this->checkIfAlreadyLoaded($moduleControllerClass);
-    // Register the module to the autoloader
-    App::get()
-      ->addAutoloadPrefix('MyTravel\\' . $this->name, 'modules\\' . $this->name);
-    $this->validateControllerClass($moduleControllerClass);
     $this->controller = call_user_func_array(array($moduleControllerClass, 'load'), array());
     // Load module configuration
     $this->status = Config::get()->modules[$this->name]['status'] ?? 'prod';
