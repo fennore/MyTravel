3bcae21557ed2fe2899f3b7a041055c012010cd6
fennore <29664945+fennore@users.noreply.github.com>
2017-08-18 17:53:19 +0200

    Application routing WIP

--- modules/Core/Controller/Routing.php
+++ modules/Core/Controller/Routing.php
@@ -2,38 +2,54 @@
 
 namespace MyTravel\Core\Controller;
 
-class Routing {
+use Symfony\Component\Routing\RouteCollection;
+use MyTravel\Core\ServiceFactoryInterface;
 
-  protected $routing;
+/**
+ * Singleton service factory for routing
+ */
+class Routing implements ServiceFactoryInterface {
+  /**
+   * Being itself
+   * @var MyTravel\Core\Controller\Routing
+   */
+  protected static $routing;
+  /**
+   * @var Symfony\Component\Routing\RouteCollection
+   */
   private $routes;
 
   protected function __construct() {
-
+    
   }
 
   /**
-   * Interact with the application.
-   * This simply returns the application object.
-   * @return App
-   */
-  public static function get() {
-    return self::$routing;
-  }
-
-  /**
-   * Load the application.
-   * This has to be called before doing anything else that
-   * concerns the application object.
+   * Short alias for App::service()->get('routes')
    * @return self
    */
-  public static function load() {
-    if (!(self::$routing instanceof Routing)) {
-      self::$routing = new Routing();
+  public static function get() {
+    return App::service()->get('routes');
+  }
+
+  /**
+   * Set as routing service
+   * @return self
+   */
+  public static function setService() {
+    if (!(self::$routing instanceof self)) {
+      self::$routing = new self();
     }
     return self::$routing;
   }
 
-  public function getRoutes() {
+  public function build() {
+    $this->routes = new RouteCollection();
+    // Dispatch event for altering application directories config node
+    $event = new RoutingEvent($subnode);
+    App::event()->dispatch('module.config.application.directories', $event);
+  }
+
+  public function routes() {
     return $this->routes;
   }
 
