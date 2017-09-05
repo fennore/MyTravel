c43fe64942669206aea31bd49e1d808724f39410
fennore <29664945+fennore@users.noreply.github.com>
2017-09-05 16:33:32 +0200

    Changes to processing timeline items

--- modules/Timeline/Model/TimelineItem.php
+++ modules/Timeline/Model/TimelineItem.php
@@ -4,12 +4,26 @@ namespace MyTravel\Timeline\Model;
 
 use MyTravel\Core\Model\SourceItem;
 use MyTravel\Core\Model\Item;
+use MyTravel\Core\Controller\Routing;
+use MyTravel\Core\Controller\App;
 
 class TimelineItem extends Item {
 
   use SourceItem;
 
-  private $property;
-  private $setting;
+  protected $property;
+  protected $setting;
+
+  /**
+   * Set the item path.
+   * Overwrite original.
+   */
+  public function setPath() {
+    $prefix = explode('/', Routing::get()
+          ->routes()
+      ->get('timeline')
+      ->getPath())[1];
+    $this->path = $prefix . '/' . App::get()->cleanPathString($this->title);
+  }
 
 }
