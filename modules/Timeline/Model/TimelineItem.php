06ade776b45079d849da7de2fe6b15745cb7e300
fennore <29664945+fennore@users.noreply.github.com>
2017-09-07 11:15:46 +0200

    Updated timeline output and implemented file link

--- modules/Timeline/Model/TimelineItem.php
+++ modules/Timeline/Model/TimelineItem.php
@@ -2,17 +2,58 @@
 
 namespace MyTravel\Timeline\Model;
 
+use Throwable;
 use MyTravel\Core\Model\SourceItem;
 use MyTravel\Core\Model\Item;
+use MyTravel\Core\Model\File;
 use MyTravel\Core\Controller\Routing;
 use MyTravel\Core\Controller\App;
 
 class TimelineItem extends Item {
 
-  use SourceItem;
+  use SourceItem {
+    setFile as sourceItemSetFile;
+  }
 
   protected $property;
   protected $setting;
+  protected $imagepath;
+
+  public function setFile(File $file) {
+    $this->sourceItemSetFile($file);
+    // Add exif data
+    try {
+      $this->property = (object) array();
+      $exif = @\exif_read_data($this->file->getFullSource(), 'FILE,COMPUTED', true, false);
+      $check = array('FILE' => NULL, 'COMPUTED' => NULL);
+      $this->property->exif = array_intersect_key($exif, $check);
+    } catch (Throwable $ex) {
+      // @todo warn user they should not use special characters for image titles
+    }
+    // Created date should match date of picture taken
+    if (isset($exif['EXIF']['DateTimeOriginal'])) {
+      $this->created = strtotime($exif['EXIF']['DateTimeOriginal']);
+    } else if (isset($exif['IFD0']['DateTime'])) {
+      $this->created = strtotime($exif['IFD0']['DateTime']);
+    }
+  }
+
+  public function __isset($name) {
+    if ($name === 'imagepath') {
+      return true;
+    }
+    return parent::__isset($name);
+  }
+
+  public function __get($name) {
+    if ($name === 'property' || $name === 'setting') {
+      return (object) $this->property;
+    }
+    if ($name === 'imagepath' && !isset($this->$name)) {
+      return $this->getNewImagePath();
+    }
+    return parent::__get($name);
+  }
 
   /**
    * Set the item path.
@@ -23,7 +64,12 @@ class TimelineItem extends Item {
           ->routes()
       ->get('timeline')
       ->getPath())[1];
-    $this->path = $prefix . '/' . App::get()->cleanPathString($this->title);
+    $this->path = trim($prefix . '/' . App::get()->cleanPathString($this->title), '/');
+    $this->imagepath = $this->getNewImagePath();
+  }
+
+  protected function getNewImagePath() {
+    return trim('img/' . App::get()->cleanPathString($this->title), '/');
   }
 
 }
