33384927a817c15490626e47e0919e368170362f
fennore <29664945+fennore@users.noreply.github.com>
2017-09-08 00:47:04 +0200

    Fix bigint on primary key and dirty fix for db sync

--- modules/Core/Controller/Db.php
+++ modules/Core/Controller/Db.php
@@ -5,6 +5,8 @@ namespace MyTravel\Core\Controller;
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\Tools\SchemaTool;
 use Doctrine\ORM\EntityManager;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Schema\Schema;
 use MyTravel\Core\ServiceFactoryInterface;
 use MyTravel\Core\Model\Module;
 use MyTravel\Core\Event\DbServiceEvent;
@@ -89,14 +91,73 @@ final class Db implements ServiceFactoryInterface {
     // and look for best cross-module implementation
     return Setup::createYAMLMetadataConfiguration($paths, App::get()->inDevelopment());
   }
+
+  /**
+   * Tweak schema diff removing some items that appear,
+   * even when nothing changed.
+   * This may in some cases clean too much.
+   * @return type
+   */
+  protected function diffSafeSql(AbstractPlatform $platform, Schema $fromSchema, Schema $toSchema) {
+    $schemaDiff = \Doctrine\DBAL\Schema\Comparator::compareSchemas($fromSchema, $toSchema);
+    // Crudely remove some diff items that always appear even when nothing changed
+    foreach ($schemaDiff->changedTables as $table) {
+      if (count($table->addedForeignKeys) === 2) {
+        $table->addedForeignKeys = array();
+      }
+      if (!empty($table->changedColumns['id']->changedProperties)) {
+        $k = array_search('autoincrement', $table->changedColumns['id']->changedProperties);
+        unset($table->changedColumns['id']->changedProperties[$k]);
+      }
+      if (empty($table->changedColumns['id']->changedProperties) && isset($table->changedColumns['id'])) {
+        unset($table->changedColumns['id']);
+      }
+    }
+    return $schemaDiff->toSaveSql($platform);
+  }
+
   /**
    * Synchronize database with newest database structure from code.
+   * Run forceSync when some changes do not get through this crude cleaned version.
    * @todo Only run this when there are changes.
    *  => something wrong with default comparator always dropping and recreating all
+   * @todo Look for updated DBAL version or fix for compare with class table inheritance?
    * @param type $name
    * @return $this
    */
   protected function sync($name = 'sqlite') {
+    if (!App::get()->inDevelopment()) {
+      return;
+    }
+    // Connection
+    $connection = $this->connection[$name]->getConnection();
+    // Gather all database mapping
+    $metaDataClassList = $this->connection[$name]
+      ->getMetadataFactory()
+      ->getAllMetadata();
+    $fromSchema = $connection
+      ->getSchemaManager()
+      ->createSchema();
+    // Create schema from yml mapping files
+    $tool = new SchemaTool($this->connection[$name]);
+    $toSchema = $tool->getSchemaFromMetadata($metaDataClassList);
+    //
+    $updateSchemaSql = $this->diffSafeSql(
+      $connection->getDatabasePlatform(), $fromSchema, $toSchema
+    );
+
+    foreach ($updateSchemaSql as $sql) {
+      $connection->executeQuery($sql);
+    }
+    return $this;
+  }
+
+  /**
+   * Do a dumb brute force sync.
+   * Only run this when something goes wrong with sync().
+   * @param type $name
+   */
+  protected function forceSync($name = 'sqlite') {
     if (!App::get()->inDevelopment()) {
       return;
     }
