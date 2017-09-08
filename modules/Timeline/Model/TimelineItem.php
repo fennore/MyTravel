21fe019e0fad0c6bbfe2a06757e029338fd3b740
fennore <29664945+fennore@users.noreply.github.com>
2017-09-08 12:15:34 +0200

    Updated TimelineItem to implement SourceItemInterface

--- modules/Timeline/Model/TimelineItem.php
+++ modules/Timeline/Model/TimelineItem.php
@@ -4,8 +4,10 @@ namespace MyTravel\Timeline\Model;
 
 use MyTravel\Core\Model\Item;
 use MyTravel\Core\ImageSource;
+use MyTravel\Core\SourceItemInterface;
 
-class TimelineItem extends Item {
+class TimelineItem extends Item implements SourceItemInterface {
+
   const MIMEMATCH = 'image/%';
 
   use ImageSource;
